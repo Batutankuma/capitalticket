c78cf09aa817572d95a1eb9a91be4bace0b00d33
batuta <saelbatita@gmail.com>
2023-03-23 05:09:26 +0100

    la surité avec token

---
+++ middlewares/security.js
@@ -0,0 +1,20 @@
+const { verify } = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization;
+        if (token) {
+            const decodedToken = verify(token, process.env.SECRET_KEY);
+            const userId = decodedToken.userId;
+            if (userId) {
+                throw new Error('Invalid user ID');
+            } else {
+                next();
+            }
+        } else {
+            res.send('token is not found');
+        }
+    } catch (e) {
+        res.send('Invalid request!');
+    }
+};
