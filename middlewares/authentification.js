1d542b9f056be17c7d590bf716c4fac55c5e8640
batuta <saelbatita@gmail.com>
2023-03-24 06:22:54 +0100

    change la date d'expiration de jwt

--- middlewares/authentification.js
+++ middlewares/authentification.js
@@ -22,7 +22,7 @@ class Authentification {
             if (!userExist) throw new Error("Désolé Email ou Mot de Passe est incorrect!");
             const isValidePwd = compareSync(password, userExist.password);
             if (!isValidePwd) throw new Error("Désolé votre mot de pass ou password est incorrect!");
-            let result = { key: sign({ userExist }, process.env.SECRET_KEY, { expiresIn: "24h" }), id_user: userExist.id };
+            let result = { key: sign({ userExist }, process.env.SECRET_KEY, { expiresIn: "1d" }), id_user: userExist.id };
             return result;
         } catch (error) {
             throw error;
